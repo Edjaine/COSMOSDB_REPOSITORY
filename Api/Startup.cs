fb26d57f94f517ed1db947d4f28aa08578fc7d01
Edjaine <edjaine-consultor@outlook.com>
2020-11-21 10:47:40 -0300

    Atualização para .NET 5.0

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -12,9 +12,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Pessoa.Infrastructure.Data;
 using TodoService.Api.Options;
+using Microsoft.OpenApi.Models;
 
 namespace Api
 {
@@ -40,13 +40,13 @@ namespace Api
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "teste", Version = "v1" });
             });
 
-            services.AddControllersWithViews()
-                .AddNewtonsoftJson(options =>
-                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-);
+            // services.AddControllersWithViews()
+            //     .AddNewtonsoftJson(options =>
+            //      options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            //);
 
             services.AddCosmosDb(serviceEndpoint, authKey, databaseName, collectionNames);
             services.AddScoped<IPessoaRepository, PessoaRepository>();
@@ -59,16 +59,13 @@ namespace Api
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();                
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "teste v1"));
             }
 
             app.UseRouting();
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
 
             app.UseAuthorization();
 
